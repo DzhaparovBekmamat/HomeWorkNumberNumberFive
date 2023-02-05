2fa2d3095fadb5b57161aa151de99ac2e616d715
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-05 23:54:01 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boss boss1 = new Boss();
+        boss1.setHealth(700);
+        boss1.setDamage(70);
+        boss1.setTypeOfProtection("Love");
+        System.out.println(boss1.getHealth() + " " + boss1.getDamage() + " " + boss1.getTypeOfProtection());
+
+        Hero[] heroes = createHeroes();
+        for (int i=0; i<heroes.length; i++) {
+            System.out.println(createHeroes()[i].getHealth()+ " " +createHeroes()[i].getDamage()+ " " +createHeroes()[i].getSuper_ability());
+        }
+        }
+    public static Hero [] createHeroes () {
+        Hero batman = new Hero(300, 35, "Fire");
+        Hero superman = new Hero(270, 30, "Ground");
+        Hero aquaman = new Hero(250, 28, "Water");
+        Hero [] heroes = {batman, superman, aquaman};
+        return heroes;
     }
 }
